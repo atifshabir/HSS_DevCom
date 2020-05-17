3f2c3704a1aad09057e2644bd6d4ac8da1abfb74
atifshabir <atif_libran@hotmail.com>
2020-05-17 21:37:03 +0500

    Machine-info is now updated. Value of PR1 is taken from serial.

--- ArduinoWork/HSS_DevCom_v0001/HSS_DevCom_v0001.ino
+++ ArduinoWork/HSS_DevCom_v0001/HSS_DevCom_v0001.ino
@@ -5,58 +5,34 @@
 const char* ssid = "AqibLower";
 const char* password = "6459693023";
 
+int pr1SensorVal=0;
 char jsonOutput[128];
-void setup() {
-  Serial.begin(115200);
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WIFI");
 
-  while(WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-
-  Serial.println("\nConnected to WiFi network");
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
-  
-}
-
-/*
- * {
-  lat: 33.569929,
-  lon: 73.139711,
-  lab_id: 3,
-  lab_order: 'CCR',
-  pv_id: 1
-}
+void UpdateSensorValsToServer()
 {
-  "name": "projects/lab-pv-system/messages/7183543520972824821"
-}
-*/
-void loop() {
+
   if(WiFi.status() == WL_CONNECTED) {
 
     HTTPClient client;
 
-    //client.begin("http://jsonplaceholder.typicode.com/comments?id="  + String(rnd));
-    //client.begin("http://jsonplaceholder.typicode.com/posts");
-    client.begin("http://192.168.100.40:3000/api/pv-info/update");
+    client.begin("http://192.168.100.46:3000/api/machine-info/update");
     
     client.addHeader("Content-Type", "application/json");
 
-    const size_t CAPACITY = JSON_OBJECT_SIZE(5);
+    const size_t CAPACITY = JSON_OBJECT_SIZE(8);
 
     StaticJsonDocument<CAPACITY> doc;
 
     JsonObject object = doc.to<JsonObject>();
-    //object["title"] = "Suscribe to Asali";
-    object["lat"] = 33.579929;
-    object["lon"] = 73.1450;
-    object["lab_id"] = 3;
-    object["lab_order"] = "CCR";
-    object["pv_id"] = 3;
-
+    object["lab_id"] = 5;
+    object["pr1"] = pr1SensorVal;
+    object["pr2"] = 0;
+    object["dr1"] = 0;
+    object["dr2"] = 0;
+    object["ss"] = 0;
+    object["monitor"] = 0;
+    object["lastUpdatedBy"] = "devcom";
+    
     serializeJson(doc, jsonOutput);
     
     int httpCode = client.POST(String(jsonOutput));
@@ -75,6 +51,39 @@ void loop() {
   } else {
     Serial.println("Connection Lost");
   }
+}
 
-  delay(5000);
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WIFI");
+
+  while(WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("\nConnected to WiFi network");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  
+}
+
+void loop() {
+
+/*
+ * Read from serial.
+ * If any string from serial is received convert it to int and save as PR1 sernsor value. Then update the server
+ */
+  String serialReadStr = "";
+  if(Serial.available() > 0) {
+    serialReadStr = Serial.readString();
+
+    Serial.print("PR1 from serial is : ");
+    Serial.println(serialReadStr);
+
+    pr1SensorVal = serialReadStr.toInt();
+
+    UpdateSensorValsToServer();
+  }
 }
